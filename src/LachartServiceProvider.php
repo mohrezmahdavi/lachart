d265d7a456e2affdbaaddc1ec31c3b6eeb1c92e4
mahdavi <m.mahdavi@raahin.com>
2021-12-19 13:28:37 +0330

    Initial commit

---
+++ src/LachartServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Comma\Lachart;
+
+use Illuminate\Support\ServiceProvider;
+
+class LachartServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->loadViewsFrom(__DIR__.'/views', 'laravelchart');
+    }
+}
